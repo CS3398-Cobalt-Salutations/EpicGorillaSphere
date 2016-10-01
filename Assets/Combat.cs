6246c6d7b46c79971e832a828660ec3866a72fbe
Dillon Lohr <djl70@txstate.edu>
2016-09-30 23:29:02 -0500

    Add example multiplayer-compatible scene
    I followed the tutorial on
    https://docs.unity3d.com/Manual/UNetSetup.html to set up an example
    scene with multiplayer features. While it does not match the gameplay we
    will have, it does contain general-purpose networking features that we
    will need to include in our product.

---
+++ Assets/Combat.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class Combat : NetworkBehaviour
+{
+    public const int maxHealth = 100;
+    public bool destroyOnDeath;
+
+    [SyncVar]
+    public int health = 100;
+
+    public void TakeDamage(int amount)
+    {
+        if (!isServer) return;
+
+        health -= amount;
+        if (health <= 0)
+        {
+            if (destroyOnDeath)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                health = maxHealth;
+
+                // Called on the server, invoked on the clients
+                RpcRespawn();
+            }
+        }
+    }
+
+    [ClientRpc]
+    void RpcRespawn()
+    {
+        if (isLocalPlayer)
+        {
+            // Move back to zero location
+            transform.position = Vector3.zero;
+        }
+    }
+}
