9f23a660476b88953ef83966fcd299c16e3c184b
Dillon Lohr <djl70@txstate.edu>
2016-10-10 20:27:08 -0500

    Add Aaron's Feature
    I tried properly merging, but I decided to simply copy over Aaron's
    files in the end. It was easy, it works, and that is pretty much all
    that matters.

---
+++ Assets/Scripts/PlayerMoveSphere.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerMoveSphere : MonoBehaviour {
+
+	public float speed;
+	public static int keyCount;
+	public Text countText;
+	public Text winText;
+
+	private Rigidbody rb;
+	private int count;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		keyCount = 0;
+		SetCountText ();
+		winText.text = "";
+	}
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject.CompareTag ("Collectible")) 
+		{
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText ();
+		}
+    }
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 5)
+		{
+			winText.text = "You Win!";
+		}
+	}
+}
