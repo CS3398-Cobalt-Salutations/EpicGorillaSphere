58118fa3348f88f4050b63577e47895f0d8f1d5c
Douglas <d_p113@txstate.edu>
2016-10-12 00:07:09 -0500

    Respawn, Hazard, and Checkpoint Prefabs
    For the love of Harambe, please don’t break anything. It all works so
    well on my local computer.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed;
+	private Rigidbody rb;
+	private Vector3 checkpoint;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.tag == "Checkpoint") 
+		{
+			checkpoint = transform.position;
+		}
+			
+		else if(other.gameObject.tag == "Hazard") 
+		{
+			transform.position = checkpoint;
+		}
+
+		else if (other.gameObject.tag == "Fallout") 
+		{
+			transform.position = checkpoint;
+		} 
+	}
+}
