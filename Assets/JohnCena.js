d12eccd75ab41f82d5af4a748561ab6635fc3648
Douglas <d_p113@txstate.edu>
2016-11-29 22:01:03 -0600

    Aaron added JOHN CENA audio to victory window for both stages. Also edited camera for teleporter maze.

---
+++ Assets/JohnCena.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+var victory : AudioClip;
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnTriggerEnter() {
+
+GetComponent(AudioSource).PlayOneShot(victory, 2.0);
+
+}
