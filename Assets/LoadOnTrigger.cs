0baf95baf1b1a43e4e67cb47bae76bc067028d1f
Stephen Phipps <S_P272@txstate.edu>
2016-12-06 03:05:03 -0600

    Level Select Good to go
    Level Select is been tweaked for the demo.

---
+++ Assets/LoadOnTrigger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadOnTrigger : MonoBehaviour
+{
+    public int LevelNumber;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter(Collider collider)
+    {
+        SceneManager.LoadScene(LevelNumber);
+    }
+}
