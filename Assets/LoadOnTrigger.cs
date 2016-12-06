6f232cf0c490b9473bc6a63508edc5f885fde7c9
Stephen Phipps <S_P272@txstate.edu>
2016-12-06 17:03:57 -0600

    commit

---
+++ Assets/LoadOnTrigger.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadOnTrigger : MonoBehaviour
+{
+    public string Trigger;
+    public int LevelNumber;
+    Animation Spin;
+    Collider Character;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter(Collider collider)
+    {
+
+        StartCoroutine(VictorySpin(collider));
+      
+    }
+
+    IEnumerator VictorySpin(Collider collider) {
+        
+        
+        yield return new WaitForSeconds(10);
+        SceneManager.LoadScene(LevelNumber);
+    }
+}
